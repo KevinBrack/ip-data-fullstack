8ab3ba2c7eb44c14f2e475cdde6651447b338d6a
Kevin <brackkevin@gmail.com>
2019-07-13 17:43:39 -0500

    Removed test EC2 container from plan

--- terraform/01_init.tf
+++ terraform/01_init.tf
@@ -3,10 +3,4 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "wb" {
-  ami                         = "ami-0986c2ac728528ac2"
-  instance_type               = "t2.micro"
-  subnet_id                   = "subnet-05e77cd6231c44dfb"
-  associate_public_ip_address = true
-}
 
