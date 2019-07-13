681bcb4bd6a1bee9ed6d627e034751d0003b6f32
Kevin <brackkevin@gmail.com>
2019-07-13 15:16:30 -0500

    Created a test resource in the previously made VPC

--- terraform/init.tf
+++ terraform/init.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-  profile = "ipchallenge"
+  profile = "default"
   region  = "us-east-2"
 }
 
-resource "aws_instance" "test-ec2" {
-  ami           = "ami-0606a0d9f566249d3"
-  instance_type = "t2.micro"
+resource "aws_instance" "wb" {
+  ami                         = "ami-0986c2ac728528ac2"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-05e77cd6231c44dfb"
+  associate_public_ip_address = true
 }
