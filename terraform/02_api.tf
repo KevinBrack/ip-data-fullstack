134abe491a2471e641d59b55494910b35d59670f
Kevin <brackkevin@gmail.com>
2019-07-14 14:35:01 -0500

    Added a deployment

--- terraform/02_api.tf
+++ terraform/02_api.tf
@@ -1,5 +1,10 @@
 resource "aws_api_gateway_rest_api" "IpApi" {
-  name = "IpApi"
-  description="API for ip information"
+  name        = "IpApi"
+  description = "API for ip information"
 }
 
+resource "aws_api_gateway_deployment" "IpApiDeployment" {
+  depends_on  = ["aws_api_gateway_integration.int_root_post"]
+  rest_api_id = "${aws_api_gateway_rest_api.IpApi.id}"
+  stage_name  = "experimental"
+}
