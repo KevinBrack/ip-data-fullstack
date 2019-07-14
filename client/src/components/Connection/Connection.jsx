c7d95d48f566f08db1de48a9b5245c0a7aa5d390
Kevin <brackkevin@gmail.com>
2019-07-14 18:57:30 -0500

    Added component to test the connection to the serverless back end

---
+++ client/src/components/Connection/Connection.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, Fragment } from "react";
+// import axios from "axios";
+
+const API_URL =
+  "https://1gr9avh006.execute-api.us-east-2.amazonaws.com/experimental/root";
+
+export default () => {
+  const [response, setresponse] = useState("Not yet...");
+  useEffect(() => {
+    async function fetchData() {
+      const apiRes = await fetch(API_URL);
+      const data = await apiRes.json();
+      return data;
+    }
+    fetchData().then(res => {
+      // console.log("RES", res);
+      setresponse(res);
+    });
+    // setresponse(data);
+    // console.log("FETCHED: ", data);
+  }, []);
+  return <Fragment>{response}</Fragment>;
+};
