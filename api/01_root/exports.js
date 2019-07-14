efdc0197e8476be0e5b1b01d5372bfb8559037ab
Kevin <brackkevin@gmail.com>
2019-07-14 11:47:46 -0500

    Set propper response structure for root endpoint

--- api/01_root/exports.js
+++ api/01_root/exports.js
@@ -1,3 +1,18 @@
-exports.handler = async function(event, context) {
-  return { "message": "Hello World!" };
+exports.handler = (event, context, callback) => {
+
+  var responseBody = {
+      "key3": "value3",
+      "key2": "value2",
+      "key1": "value1"
+  };
+
+  var response = {
+      "statusCode": 200,
+      "headers": {
+          "my_header": "my_value"
+      },
+      "body": JSON.stringify(responseBody),
+      "isBase64Encoded": false
+  };
+  callback(null, response);
 };
