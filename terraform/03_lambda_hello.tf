4cac9114ad4e2259502209627a1c35c4e8a4e8be
Kevin <brackkevin@gmail.com>
2019-07-14 11:16:30 -0500

    Restructuring and renaming now that I understand what Resource is

--- terraform/03_lambda_hello.tf
+++ terraform/03_lambda_hello.tf
@@ -1,9 +1,9 @@
-resource "aws_lambda_function" "ip_hello_world" {
-  filename      = "../api/01_hello_world/ip_hello_world_payload.zip"
-  function_name = "ip_hello_world"
+resource "aws_lambda_function" "ip_root" {
+  filename      = "../api/01_root/ip_root_payload.zip"
+  function_name = "ip_root"
   role          = "${aws_iam_role.iam_for_ip_lambda.arn}"
   handler       = "exports.handler"
   runtime       = "nodejs10.x"
 
-  source_code_hash = "${filebase64sha256("../api/01_hello_world/ip_hello_world_payload.zip")}"
+  source_code_hash = "${filebase64sha256("../api/01_root/ip_root_payload.zip")}"
 }
--- terraform/04_api_gateway.tf
+++ terraform/04_api_gateway.tf
@@ -8,8 +8,8 @@ resource "aws_api_gateway_rest_api" "ip_api" {
 }
 
 # RESOURCE
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "resource"
+resource "aws_api_gateway_resource" "root" {
+  path_part   = "root"
   parent_id   = "${aws_api_gateway_rest_api.ip_api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.ip_api.id}"
 }
@@ -17,7 +17,7 @@ resource "aws_api_gateway_resource" "resource" {
 # METHOD
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.ip_api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  resource_id   = "${aws_api_gateway_resource.root.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
@@ -25,11 +25,11 @@ resource "aws_api_gateway_method" "method" {
 # INTEGRATION 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.ip_api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  resource_id             = "${aws_api_gateway_resource.root.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.myregion}:lambda:path/2015-03-31/functions/${aws_lambda_function.ip_hello_world.arn}/invocations"
+  uri                     = "arn:aws:apigateway:${var.myregion}:lambda:path/2015-03-31/functions/${aws_lambda_function.ip_root.arn}/invocations"
 }
 
 # Lambda
@@ -41,10 +41,11 @@ resource "aws_api_gateway_integration" "integration" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.ip_hello_world.function_name}"
+  function_name = "${aws_lambda_function.ip_root.function_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.ip_api.id}:${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn    = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.ip_api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.root.path}"
   # ^^^ This line failing currently
+  # source_arn = "${aws_api_gateway_rest_api.ip_api.execution_arn}/*/*/*"
 }
 
 # LOAD BALANCER
