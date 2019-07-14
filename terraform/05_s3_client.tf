e2891e58a97f3c1a79bba07761c8c3a218e66a38
Kevin <brackkevin@gmail.com>
2019-07-14 16:20:36 -0500

    Successfully created bucket for investigateip with ability to host static site

---
+++ terraform/05_s3_client.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "investigateip" {
+  bucket = "investigateip"
+  acl    = "public-read"
+  policy = "${file("policy_json/s3_policy.json")}"
+
+  website {
+    index_document = "index.html"
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://1gr9avh006.execute-api.us-east-2.amazonaws.com", "https://www.investigateip.net"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
