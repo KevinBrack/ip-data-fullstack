72ffa0d066a0ca8b20bd78fab0539a1803c76fe1
Kevin <brackkevin@gmail.com>
2019-07-14 01:16:59 -0500

    rearanged arguments

--- terraform/03_lambda_hello.tf
+++ terraform/03_lambda_hello.tf
@@ -3,9 +3,7 @@ resource "aws_lambda_function" "ip_hello_world" {
   function_name = "ip_hello_world"
   role          = "${aws_iam_role.iam_for_ip_lambda.arn}"
   handler       = "exports.handler"
+  runtime       = "nodejs10.x"
 
   source_code_hash = "${filebase64sha256("../api/01_hello_world/ip_hello_world_payload.zip")}"
-
-  runtime = "nodejs10.x"
-
 }
