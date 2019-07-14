faef8af7d020cb6c503af01223aafd16a913652b
Kevin <brackkevin@gmail.com>
2019-07-14 13:21:54 -0500

    Made new lambda role and reattached root lambda

---
+++ terraform/04_api_01_root.tf
@@ -0,0 +1,23 @@
+variable "zip_01_root" {
+  default = "../api/01_root/ip_root_payload.zip"
+}
+
+resource "aws_lambda_function" "root" {
+  filename      = "${var.zip_01_root}"
+  function_name = "ip_root"
+  role          = "${aws_iam_role.ip_lambda_role.arn}"
+  handler       = "exports.handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("${var.zip_01_root}")}"
+
+  runtime = "nodejs8.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
