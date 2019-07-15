2f569d4dfc5cf3a175bf2cd10e496c01045d83d4
Kevin <brackkevin@gmail.com>
2019-07-14 19:40:11 -0500

    Added declaration for virusTotalKey

--- terraform/00_variables.tf
+++ terraform/00_variables.tf
@@ -5,3 +5,5 @@ variable "myregion" {
 # not sure if safe to have in source so placed in "../private/secret.tfvars"
 # include -var-file="../private/secret.tfvars" with `terraform plan` and `terraform apply`
 variable "accountId" {}
+
+variable "virusTotalKey" {}
