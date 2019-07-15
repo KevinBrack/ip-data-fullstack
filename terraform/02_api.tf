ee4e50dd652fdd79acc86c2ed401e5654bef9b31
Kevin <brackkevin@gmail.com>
2019-07-14 20:24:24 -0500

    Changed name of deployment because terraform was not recognizing changes

--- terraform/02_api.tf
+++ terraform/02_api.tf
@@ -4,7 +4,7 @@ resource "aws_api_gateway_rest_api" "IpApi" {
 }
 
 resource "aws_api_gateway_deployment" "IpApiDeployment" {
-  depends_on  = ["aws_api_gateway_integration.int_root_post"]
+  depends_on  = ["aws_api_gateway_integration.int_root_post", "aws_api_gateway_integration.int_ip_scan_post"]
   rest_api_id = "${aws_api_gateway_rest_api.IpApi.id}"
-  stage_name  = "experimental"
+  stage_name  = "experimental_2"
 }
